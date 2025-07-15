6f4a8b23056aab72ff12c8b56c6752900fe4cd4b
Matthew Aitken <matt@itken.net>
2025-07-15 17:24:15 +0100

    Add summary details

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from "react";
 
 import QUESTIONS from '../questions.js';
 import Question from "./Question.jsx";
-import completeImage from '../assets/quiz-complete.png'
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
     const [ userAnswers, setUserAnswers ] = useState([]);
@@ -24,10 +24,9 @@ export default function Quiz() {
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
     if (quizIsComplete) {
-        return <div id="summary">
-            <img src={completeImage} alt="completed quiz image"/>
-            <h2>Quiz Completed!</h2>
-        </div>
+        return <Summary
+            userAnswers={userAnswers}
+        />
     }
     return (
         <div id="quiz">
