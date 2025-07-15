d55fa119ddb85c8dc89a2f6aed5e4aa312c6b83d
Matthew Aitken <matt@itken.net>
2025-07-15 14:42:06 +0100

    Add logic to show end of quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import QUESTIONS from '../questions.js';
+import completeImage from '../assets/quiz-complete.png'
 
 export default function Quiz() {
     const [ userAnswers, setUserAnswers ] = useState([]);
@@ -8,20 +9,33 @@ export default function Quiz() {
     // Using userAnswers to derive the current question as the current
     // question will be the length of the number of answered questions
     // as an index for an array.
-    const currentQuestionIndex = userAnswers.length;
-
+    const currentQuestionIndex = userAnswers.length;    
+    const quizIsComplete = currentQuestionIndex === QUESTIONS.length;
+    
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
     }
+    
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={completeImage} alt="completed quiz image"/>
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+    // Create a new array from the answers to be able to shuffle them without
+    // editing the original array which we need in order still.
+    const shuffledAnswers = [...QUESTIONS[currentQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[currentQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[currentQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
