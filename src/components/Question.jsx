aa11d2c7f7b7325f4378a9917bda72dd2163e1ad
Matthew Aitken <matt@itken.net>
2025-07-15 16:56:58 +0100

    Update timers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -10,6 +10,16 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
         isCorrect: null
     });
 
+    let timer = 10000;
+
+    if (answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if (answer.isCorrect !== null) {
+        timer = 2000;
+    }
+
     function handleSelectAnswer(answer) {
         setAnswer({
             selectedAnswer: answer,
@@ -38,8 +48,10 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
     return (
         <div id="question">
             <QuestionTimer
-                timeout={10000}
-                onTimeout={onSkipAnswer}
+                key={timer}
+                timeout={timer}
+                onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+                mode={answerState}
             />
             <h2>{QUESTIONS[questionIndex].text}</h2>
             <Answers
